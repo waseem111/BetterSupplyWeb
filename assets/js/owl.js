e46b363c2a5f35dedf5b047c821fbc57e75d9a2f
waseem111 <abdul.waseem@ibsksa.com>
2020-08-31 21:02:57 +0530

    1 commite
    1 commite

---
+++ assets/js/owl.js
@@ -0,0 +1,41 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:5,
+    loop:true,
+    /*margin:10,*/
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[2000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+
+
+
+/*$('.owl-carousel').owlCarousel({
+    autoPlay: 1,
+    loop: true,
+    margin: 0,
+    nav: true,
+    navText: [
+        "<i class='fa fa-caret-left'></i>",
+        "<i class='fa fa-caret-right'></i>"
+    ],
+   autoPlay: 1,
+    autoplayHoverPause: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})*/
